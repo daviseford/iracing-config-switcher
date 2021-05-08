589a092b2da3f98fa1eab78d9efe589fa7360e4d
Davis Ford <daviseford@users.noreply.github.com>
2021-05-07 23:23:26 -0400

    Update to_party.bat

--- to_party.bat
+++ to_party.bat
@@ -3,7 +3,10 @@ copy /y "C:\Users\davis\Documents\iRacing\app.ini" app.ini.bak
 copy /y "C:\Users\davis\Documents\iRacing\rendererDX11.ini" rendererDX11.ini.bak
 
 @REM Copy our tracked versions of the settings to overwrite
-copy /y personal_app.ini "C:\Users\davis\Documents\iRacing\app.ini"
+copy /y party_app.ini "C:\Users\davis\Documents\iRacing\app.ini"
 copy /y rendererDX11.ini "C:\Users\davis\Documents\iRacing\rendererDX11.ini"
+
+@REM Start the iRacing programs
+START start_programs.bat
 
 exit
