c28050b39b1ccba88ada8b0c50f7516dcc945b5b
Davis Ford <daviseford@users.noreply.github.com>
2021-05-07 23:18:10 -0400

    Create to_party.bat

---
+++ to_party.bat
@@ -0,0 +1,9 @@
+@REM Copy our existing files as a backup precaution
+copy /y "C:\Users\davis\Documents\iRacing\app.ini" app.ini.bak
+copy /y "C:\Users\davis\Documents\iRacing\rendererDX11.ini" rendererDX11.ini.bak
+
+@REM Copy our tracked versions of the settings to overwrite
+copy /y personal_app.ini "C:\Users\davis\Documents\iRacing\app.ini"
+copy /y rendererDX11.ini "C:\Users\davis\Documents\iRacing\rendererDX11.ini"
+
+exit
