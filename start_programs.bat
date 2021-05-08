328b9294185ac953caedbb88ee6bb8937e631b30
Davis Ford <daviseford@users.noreply.github.com>
2021-05-07 23:18:06 -0400

    Adding party mode

---
+++ start_programs.bat
@@ -0,0 +1,7 @@
+start "" "D:\Games\iRacing\ui\iRacingUI.exe
+start "" "D:\Programs\iSpeed\iSpeed.exe
+start "" "D:\Games\CrewChief\CrewChiefV4.exe
+start "" "D:\Program Files (x86)\Rhinode LLC\Trading Paints\Trading Paints.exe"
+start "" "D:\Program Files (x86)\JoyToKey\JoyToKey.exe"
+
+exit
--- to_personal.bat
+++ to_personal.bat
@@ -1,4 +1,12 @@
+@REM Copy our existing files as a backup precaution
 copy /y "C:\Users\davis\Documents\iRacing\app.ini" app.ini.bak
+copy /y "C:\Users\davis\Documents\iRacing\rendererDX11.ini" rendererDX11.ini.bak
+
+@REM Copy our tracked versions of the settings to overwrite
 copy /y personal_app.ini "C:\Users\davis\Documents\iRacing\app.ini"
+copy /y rendererDX11.ini "C:\Users\davis\Documents\iRacing\rendererDX11.ini"
+
+@REM Start the iRacing programs
+START start_programs.bat
 
 exit
