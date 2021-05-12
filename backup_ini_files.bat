2fbf11ddf47eb7f95c1a635ec9a0cdcf9fbc7ea8
Davis Ford <daviseford@users.noreply.github.com>
2021-05-12 12:53:52 -0400

    Tweaking ini files

---
+++ backup_ini_files.bat
@@ -0,0 +1,8 @@
+@REM Copy our existing files as a backup precaution
+copy /y "C:\Users\davis\Documents\iRacing\app.ini" app.ini.bak
+copy /y "C:\Users\davis\Documents\iRacing\rendererDX11.ini" rendererDX11.ini.bak
+copy /y "C:\Users\davis\Documents\iRacing\core.ini" core.ini.bak
+
+echo "Backups created successfully."
+
+exit
--- to_personal.bat
+++ to_personal.bat
@@ -1,10 +1,10 @@
 @REM Copy our existing files as a backup precaution
-copy /y "C:\Users\davis\Documents\iRacing\app.ini" app.ini.bak
-copy /y "C:\Users\davis\Documents\iRacing\rendererDX11.ini" rendererDX11.ini.bak
+START backup_ini_files.bat
 
 @REM Copy our tracked versions of the settings to overwrite
 copy /y personal_app.ini "C:\Users\davis\Documents\iRacing\app.ini"
 copy /y rendererDX11.ini "C:\Users\davis\Documents\iRacing\rendererDX11.ini"
+copy /y core.ini "C:\Users\davis\Documents\iRacing\core.ini"
 
 @REM Start the iRacing programs
 START start_programs.bat
